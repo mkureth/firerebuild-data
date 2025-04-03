b01500ad0cd4eb5046b63dea57ec3ad240cd9e47
Mike Kureth <mkureth@gmail.com>
2025-04-03 11:29:54 -0700

    readme

--- scrape/weather-daily/README.md
+++ scrape/weather-daily/README.md
@@ -1,2 +1,12 @@
-# weather
-Repo for weather data
+# weather-daily
+Repo for weather-daily data
+
+# node version:
+18.18.0
+
+# research
+https://www.wunderground.com/history/daily/us/ca/los-angeles/KLAX/date/2025-1-7
+
+https://weatherspark.com/h/d/1705/2025/1/7/Historical-Weather-on-Tuesday-January-7-2025-in-Los-Angeles-California-United-States#Figures-WindSpeed
+
+https://weatherspark.com/h/d/1705/2025/1/7/Historical-Weather-on-Tuesday-January-7-2025-in-Los-Angeles-California-United-States#Figures-Temperature
--- scrape/weather-monthly/a-download.js
+++ scrape/weather-monthly/a-download.js
@@ -35,13 +35,3 @@ const filename = `data/html/${yearMonth}.html`;
 
 // Call the function to download and save the HTML file
 downloadHTML(url, filename);
-
-/* RESEARCH
-https://api.weather.com/v3/wx/observations/current?apiKey=e1f10a1e78da46f5b10a1e78da96f525&a;geocode=33.96,-118.4&a;units=e&a;language=en-US&a;format=json
-https://www.wunderground.com/history/monthly/us/ca/los-angeles/KLAX/date/2024-1
-https://www.wunderground.com/history/monthly/us/ca/los-angeles/KLAX/date/2024-1
-
-https://weatherspark.com/download/1705/Download-Los-Angeles-California-United-States-Weather-Data
-https://weatherspark.com/h/d/1705/2025/1/20/Historical-Weather-on-Monday-January-20-2025-in-Los-Angeles-California-United-States#Figures-WindSpeed
-https://weatherspark.com/h/d/1705/2025/1/7/Historical-Weather-on-Tuesday-January-7-2025-in-Los-Angeles-California-United-States#Figures-WindSpeed
-*/
